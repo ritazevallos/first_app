6ff3f8f4eb22690d84f378593f39c2a639d6c4cc
Rita Zevallos <rzevall1@swarthmore.edu>
2013-12-30 03:33:13 -0500

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,20 +10,29 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
-    @user = User.new
+    if signed_in?
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    if signed_in?
+      redirect_to root_path
     else
-      render 'new'
+      @user = User.new(user_params)
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
     end
   end
 
@@ -57,13 +66,6 @@ class UsersController < ApplicationController
     end
 
   # Before filters
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
 
   def correct_user
     @user = User.find(params[:id])
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,7 +12,6 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
-      #it { should_not have_link('Settings', href: edit_user_path(user))}
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -34,6 +33,10 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in')}
+        it { should_not have_link('Users',       href: users_path)}
+        it { should_not have_link('Profile',     href: user_path(user)) }
+        it {should_not have_link('Settings',     href: edit_user_path(user))}
+        it { should_not have_link('Sign out',    href: signout_path) }
       end
     end
   end
@@ -55,8 +58,10 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
         end
       end
+
 
       describe "in the Users controller" do
 
@@ -73,6 +78,19 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in')}
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path)}
+        end
+
+        describe "submitting to the destroy action" do
+          before {delete micropost_path(FactoryGirl.create(:micropost))}
+          specify {expect(response).to redirect_to(signin_path)}
         end
       end
     end
